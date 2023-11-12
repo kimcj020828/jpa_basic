d2e801e6103945dd018813dc5e8d88a8d72ad854
kimcj020828m <kimcj020828@gmail.com>
2023-11-12 23:04:17 +0900

    영속성 컨텍스트
    - 쓰기지연
    - 플러시
    - 준영속상태 내용 추가

--- 03_Persistence_management/ex2-persistence-context-jpa/src/main/java/JpaPersistenceContextMain.java
+++ 03_Persistence_management/ex2-persistence-context-jpa/src/main/java/JpaPersistenceContextMain.java
@@ -43,17 +43,47 @@ public class JpaPersistenceContextMain {
 //            Member member2 = em.find(Member.class, 101L);
 //            System.out.println("result=" + (member1==member2)); // 영속 엔티티의 동일성 보장(true)
 
-            /*
+//            /*
+//            트랜젝션을 지원하는 쓰기 지연
+//             */
+//            Member member1 = new Member(150L,"A");
+//            Member member2 = new Member(160L,"B");
+//
+//            em.persist(member1);
+//            em.persist(member2);
+
+//            // 객체가 변경되면 commit 외, 별도의 로직 처리는 필요x
+//            final Member findMember = em.find(Member.class, 150L);
+//            findMember.setName("ZZZZZZ");
+//
+//            // em.persist(findMember); 의미 없는 코드
             
+            /*
+            Flush 강제 호출
              */
+//            Member member = new Member(200L, "200Member");
+//            em.persist(member);
+//
+//            em.flush();
 
+            /*
+             * 영속 -> 준영속
+             */
+            Member findMember = em.find(Member.class, 150L);
 
+            findMember.setName("AAAAA");
+
+//            em.detach(findMember); // 특정 객체만 준영속 상태로 만듬
+            em.clear(); //영속성 컨텍스트를 모두 초기화
+            Member findMember2 = em.find(Member.class, 150L);
+
+            System.out.println("==================");
 
             tx.commit();
-        }catch (Exception e){
+        } catch (Exception e){
             System.out.println(e.getMessage());
             tx.rollback();
-        }finally {
+        } finally {
             em.close();
         }
         emf.close();
